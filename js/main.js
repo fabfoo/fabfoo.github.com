e6ea0fafc2ac5316f8a0b290ce21c88e44986e0f
Nguyen Bao Duy <duy.nguyen@enouvo.com>
2014-02-10 22:10:35 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(window).load(function() {
+	$('.flexslider').flexslider({
+		// Primary Controls
+		controlNav: false,	//Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+		directionNav: false,	//Boolean: Create navigation for previous/next navigation? (true/false)
+	});
+});
